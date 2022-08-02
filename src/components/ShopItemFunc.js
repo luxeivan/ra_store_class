657bfefc9457b04e594585e7de018967e154aa26
luxeivan <luxeivan@gmail.com>
2022-08-02 20:51:19 +0300

    first commit

---
+++ src/components/ShopItemFunc.js
@@ -0,0 +1,38 @@
+import ShopItemModels from '../models/ShopItemModels';
+import PropTypes from 'prop-types';
+import './ShopItemFunc.css'
+import React from 'react';
+
+class ShopItemFunc extends React.Component {
+    // конструктор приведён лишь для полноты картины
+    // в текущем виде он не является обязательным
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const { item } = this.props;
+        return (
+            <div className="main-content">
+                <h2>{item.brand}</h2>
+                <h1>{item.title}</h1>
+                <h3>{item.description}</h3>
+                <div className="description">
+                    {item.description}
+                </div>
+                <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+                <div className="divider"></div>
+                <div className="purchase-info">
+                    <div className="price">{item.currency}{item.price.toFixed(2)}</div>
+                    <button>Добавить в корзину</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+ShopItemFunc.propTypes = {
+    item: PropTypes.instanceOf(ShopItemModels).isRequired
+}
+
+export default ShopItemFunc;
